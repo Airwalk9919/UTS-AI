8b76894deb9d351f2a7fd064fa4a137094a8e53e
Airwalk9919 <104032994+Airwalk9919@users.noreply.github.com>
2022-04-20 03:24:53 +0700

    Add files via upload
    Nama:Rivaldy Ramadhan 
    Kelas :06TPLM003
    NIM:191011402575

---
+++ Uts_Rivaldy.py
@@ -0,0 +1,12 @@
+print("Perulangan For: ")
+siswa = ["Rivaldy", "Ari", "Bima"]
+for x in siswa:
+  print(x)
+print("")  
+print("Perulangan While:")
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i udah kgk lebih besar dari 6")
